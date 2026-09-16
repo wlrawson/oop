e27b36146f6bc263f65c41c0b4d8d9891cbfe756
wlrawson <wrawson@rawson-inc.com>
2026-09-16 09:42:39 -0500

    Add files via upload

---
+++ 9-16_practice.py
@@ -0,0 +1,10 @@
+mydictionary = {"name1":"Will",
+                "name2":"Isaac",
+                "name3":"Bob"
+                }
+
+print (mydictionary)
+
+mydictionary.update({"name4":"James"})
+mydictionary.update({"name5":"Peter"})
+print (mydictionary)
