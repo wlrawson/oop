9622bcd771b4ef567ba4bebe9770f53a578c066b
wlrawson <wrawson@rawson-inc.com>
2026-09-23 09:52:14 -0500

    Add files via upload

---
+++ 9-23_practice1.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def myfunction(recieved_list):
+    return(recieved_list)
+
+
+x = int(input())
+y = int(input())
+
+sum = add(x, y)
---
+++ 9-23_practice2.py
@@ -0,0 +1,7 @@
+def add (a, b):
+    print (a)
+    a= a + 10
+    print (a)
+
+myage = 100, y =200
+add(myage, y)
