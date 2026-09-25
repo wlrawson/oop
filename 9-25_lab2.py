00ffd1e04f8c72593776af776d78b0f3e142117d
wlrawson <wrawson@rawson-inc.com>
2026-09-25 15:06:02 -0500

    Add files via upload

--- 9-25_lab2.py
+++ 9-25_lab2.py
@@ -1,39 +1,59 @@
 myEmployee={
-    "Employee1":{
-        "Name": "",
-        "Basic_pay": "",
-        "Allowance": "",
-        "Deductions": "",
-        "Taxes": "",
-        "Gross_pay": "",
-        "Net_pay":"",}}
-
-
+    }
 
 def add_employee():
+    employee_id = "Employee" + str(len(myEmployee) + 1)
+
     employee_name = input("Enter Employee name: ")
-    basic_pay = input("Enter Basic pay: ")
-    allowance = input("Enter Allowance: ")
-    deductions = input("Enter Deductions: ")
-    taxes = input("Enter Taxes: ")
-    gross_pay = input("Enter Gross_pay: ")
-    net_pay = input("Enter Net pay: ")
-    myEmployee.update({"Employee1":{"name": employee_name, "basic_pay":basic_pay, "allowance":allowance, "deductions":deductions, "taxes":taxes, "gross_pay":gross_pay, "net_pay":net_pay}})
+    basic_pay = int(input("Enter Basic pay: "))
+    allowance = int(input("Enter Allowance: "))
+    deductions = int(input("Enter Deductions: "))
+    taxes = int(input("Enter Taxes: "))
+    gross_pay = (basic_pay + allowance)
+    net_pay = (gross_pay - deductions - taxes)
+
+    myEmployee.update({
+        employee_id: {
+            "name": employee_name,
+            "basic_pay": basic_pay,
+            "allowance": allowance,
+            "deductions": deductions,
+            "taxes": taxes,
+            "gross_pay": gross_pay,
+            "net_pay": net_pay
+        }
+    })
 
 def delete_employee():
-    myEmployee.pop("Employee1")
+    myEmployee.pop(input("Enter Employee to delete: "))
 
 def modify_employee():
-    remove_employee = input("Enter employee to remove: ")
-    if remove_employee in dict:
-        myEmployee.pop("Employee1")
-        add_employee()
-    else:
-        print("Employee not found")
+    myEmployee.pop(input("Enter Employee to modify: "))
+    employee_id = "Employee" + str(len(myEmployee) + 1)
+
+    employee_name = input("Enter New Employee name: ")
+    basic_pay = input("Enter New Basic pay: ")
+    allowance = input("Enter New Allowance: ")
+    deductions = input("Enter New Deductions: ")
+    taxes = input("Enter New Taxes: ")
+    gross_pay = input("Enter New Gross_pay: ")
+    net_pay = input("Enter New Net pay: ")
+
+    myEmployee.update({
+        employee_id: {
+            "name": employee_name,
+            "basic_pay": basic_pay,
+            "allowance": allowance,
+            "deductions": deductions,
+            "taxes": taxes,
+            "gross_pay": gross_pay,
+            "net_pay": net_pay
+        }
+    })
 
 def display_myemployee():
-    print(myEmployee)
-
+    for employee_id, employee in myEmployee.items():
+        print(employee_id, employee)
 
 while "true":
     print ("1 Add Employee")
@@ -50,6 +70,6 @@ while "true":
     elif choice == 3:
         modify_employee()
     elif choice == 4:
-        print(myEmployee)
+        display_myemployee()
     else:
         exit(True)
